ca3d812c721d36103ee49f44568de78cd6c301e2
0xNima <neemaiox@gmail.com>
2023-08-08 20:41:47 +0330

    make username field optional

--- apps/authenticate/serializers.py
+++ apps/authenticate/serializers.py
@@ -10,8 +10,9 @@ from apps.core.models import Wallet
 
 class RegisterUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        required=False,
+        validators=[UniqueValidator(queryset=User.objects.all())],
+        allow_null=True
     )
 
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
@@ -28,8 +29,8 @@ class RegisterUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
+            username=validated_data.get('username'),
+            email=validated_data.get('email'),
         )
         user.set_password(validated_data['password'])
         user.save()
